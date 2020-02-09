1acd264f104693f98534fe0351003e12b85bca04
connorcarson <cc3237@nyu.edu>
2020-02-09 16:57:15 -0500

    Got help from Bri understanding the Lifecycle Manager and wrote one for all players. Haven't implemented it in Unity yet. Should work in theory?

---
+++ Soccer_Project/Assets/Scripts/AIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController
+{
+    private List<Player> _players;
+    private int _numberOfPlayers = 11;
+    public void Initialize()
+    {
+        _players = new List<Player>();
+        GeneratePlayers();   
+    }
+
+    public void Update()
+    {
+        foreach (var player in _players)
+        {
+            player.Update();
+        }
+    }
+
+    private void GeneratePlayers()
+    {
+        var userObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/AI_Player"));
+        var userPlayer = new UserPlayer(userObject, Player.Team.Blue);
+        userPlayer.AssignKeys(KeyCode.K, KeyCode.K, KeyCode.K, KeyCode.K);
+        _players.Add(userPlayer);
+        
+        for (var i = 0; i < _numberOfPlayers - 1; i++)
+        {
+            var aiObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/AI_Player"));
+            _players.Add(new AIPlayer(aiObject, Player.Team.Blue));
+        }
+
+        for (var i = 0; i < _numberOfPlayers; i++)
+        {
+            var aiObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/AI_Player"));
+            _players.Add(new AIPlayer(aiObject, Player.Team.Orange));
+        }
+    }
+}
--- Soccer_Project/Assets/Scripts/Player.cs
+++ Soccer_Project/Assets/Scripts/Player.cs
@@ -1,20 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : SoccerPlayer
+public abstract class Player
 {
-    public KeyCode[] movementKeys;
-    
+    public enum Team
+    {
+        Blue,
+        Orange
+    }
+
+    protected Team team;
+    protected GameObject player;
+    private const float speed = 1.0f;
+
+    public void Update()
+    {
+        MoveTowards(Direction());
+    }
+
+    protected virtual Vector3 Direction()
+    {
+        return new Vector3();
+    }
+
+    private void MoveTowards(Vector3 direction)
+    {
+        var speedMultiplier = speed * Time.deltaTime;
+        player.transform.position += direction * speedMultiplier;
+    }
+}
+
+public class UserPlayer : Player
+{
+    private readonly KeyCode[] _movementKeys = new KeyCode[4];
+
     protected override Vector3 Direction()
     {
         var direction = new Vector3();
 
-        if(Input.GetKey(KeyCode.W)) direction += transform.forward;
-        if(Input.GetKey(KeyCode.A)) direction += -transform.right;
-        if(Input.GetKey(KeyCode.S)) direction += -transform.forward;
-        if(Input.GetKey(KeyCode.D)) direction += transform.right;
+        if (Input.GetKey(_movementKeys[0])) direction += player.transform.forward;
+        if (Input.GetKey(_movementKeys[1])) direction += -player.transform.right;
+        if (Input.GetKey(_movementKeys[2])) direction += -player.transform.forward;
+        if (Input.GetKey(_movementKeys[3])) direction += player.transform.right;
 
         return direction;
     }
+
+    public void AssignKeys(KeyCode up, KeyCode left, KeyCode down, KeyCode right)
+    {
+        _movementKeys[0] = up;
+        _movementKeys[1] = left;
+        _movementKeys[2] = down;
+        _movementKeys[3] = right;
+    }
+
+    public UserPlayer(GameObject playerGameObject, Team teamAssignment)
+    {
+        player = playerGameObject;
+        team = teamAssignment;
+    }
 }
+
+public class AIPlayer : Player
+{ 
+    protected override Vector3 Direction()
+    {
+        return ServicesLocator.Ball.transform.position - player.transform.position;
+    }
+
+    public AIPlayer(GameObject playerGameObject, Team teamAssignment)
+    {
+        player = playerGameObject;
+        team = teamAssignment;
+    }
+}
+
