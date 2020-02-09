44c6f5a7280968444eea59f4575f9c00db68eff5
connorcarson <cc3237@nyu.edu>
2020-02-09 17:42:16 -0500

    Minor refactoring.

--- Soccer_Project/Assets/Scripts/PlayerController.cs
+++ Soccer_Project/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController
 {
     private List<Player> _players;
-    private int _numberOfPlayers = 11;
+    private int _numberOfPlayers = 2;
     public void Initialize()
     {
         _players = new List<Player>();
@@ -22,7 +22,7 @@ public class PlayerController
 
     private void GeneratePlayers()
     {
-        var userObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/AI_Player"));
+        var userObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/Player"));
         userObject.transform.position = new Vector3(Random.Range(-8, 8), userObject.transform.position.y,Random.Range(-4, 4));
         var userPlayer = new UserPlayer(userObject, Player.Team.Blue);
         userPlayer.AssignKeys(KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D);
@@ -30,14 +30,14 @@ public class PlayerController
         
         for (var i = 0; i < _numberOfPlayers - 1; i++)
         {
-            var aiObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/AI_Player"));
+            var aiObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/Player"));
             aiObject.transform.position = new Vector3(Random.Range(-8, 8), aiObject.transform.position.y, Random.Range(-4, 4));
             _players.Add(new AIPlayer(aiObject, Player.Team.Blue));
         }
 
         for (var i = 0; i < _numberOfPlayers; i++)
         {
-            var aiObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/AI_Player"));
+            var aiObject = Object.Instantiate(Resources.Load<GameObject>("Prefabs/Player"));
             aiObject.transform.position = new Vector3(Random.Range(-8, 8), aiObject.transform.position.y, Random.Range(-4, 4));
             _players.Add(new AIPlayer(aiObject, Player.Team.Orange));
         }
