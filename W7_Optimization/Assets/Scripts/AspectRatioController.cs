58343d82d10877ff6e227073209ebe7e39b500c9
Connor <connor@glowup.games>
2020-03-28 15:16:28 -0400

    After some research, found a great example for controlling aspect ratio of game regardless of window size! Implemented in a demo scene with canvas and sample text for demonstrative purposes.

--- W7_Optimization/Assets/Scripts/AspectRatioController.cs
+++ W7_Optimization/Assets/Scripts/AspectRatioController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class AspectRatioController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _horizontalAspect, _verticalAspect;
+
+    private void Start()
     {
-        
+        Camera cam = GetComponent<Camera>();
+        AdjustToWindowSize(_horizontalAspect, _verticalAspect, cam);
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void AdjustToWindowSize(float horizontalAspect, float verticalAspect, Camera toAdjust)
     {
+        float targetAspectRatio = horizontalAspect / verticalAspect;
+        float windowAspectRatio = (float)Screen.width / (float)Screen.height;
+
+        float scaleHeight = windowAspectRatio / targetAspectRatio;
+        float scaleWidth = 1.0f / scaleHeight;
+
+        Rect rect = toAdjust.rect;
         
+        if (scaleHeight < 1.0f)
+        {
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+        }
+        else
+        {
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+        }
+        
+        toAdjust.rect = rect;
     }
 }
