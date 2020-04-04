6c4c03195456e3f035076e5487c414a9046916c6
Connor <connor@glowup.games>
2020-04-04 17:18:12 -0400

    WEEK 8: clean commit of new project for custom tooling.

---
+++ W8_CustomTools/Assets/Scripts/DialogueEditorWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DialogueEditorWindow : EditorWindow
+{
+    
+    [MenuItem("Window/Dialogue Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow<DialogueEditorWindow>("DialogueEditor");
+    }
+    private void OnGUI()
+    {
+        
+    }
+}
