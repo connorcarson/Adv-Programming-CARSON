71bd6a03333e6a75cfaf2fcdf0a7441a536befad
Connor <connor@glowup.games>
2020-04-12 18:21:36 -0400

    Added simple, timed animation to represent force field.

--- Soccer_Project/Assets/Scripts/ForceFieldTriggerable.cs
+++ Soccer_Project/Assets/Scripts/ForceFieldTriggerable.cs
@@ -1,14 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ForceFieldTriggerable : MonoBehaviour
 {
-    [HideInInspector] private float forceFieldRange = 1.0f;
-    [HideInInspector] private float hitForce = 250f;
-    private WaitForSeconds forceFieldDuration = new WaitForSeconds(0.5f);
+    [HideInInspector] public float forceFieldRange = 1.0f;
+    [HideInInspector] public float hitForce = 500f;
+    
+    private SpriteRenderer forceField_sr;
+    private Vector3 forceFieldDefaultScale, forceFieldMaxScale;
+    private float forceFieldDuration = 0.2f;
+    private float timeElapsed;
     private int targetLayer = 1 << 8;
 
+
+    public void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        forceField_sr = GetComponentInChildren<SpriteRenderer>();
+        forceFieldMaxScale = new Vector3(3.0f, 3.0f, 3.0f);
+        forceFieldDefaultScale = forceField_sr.transform.localScale;
+    }
+    
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -19,6 +37,8 @@ public class ForceFieldTriggerable : MonoBehaviour
 
     public void Trigger()
     {
+        StartCoroutine(FieldEffect());
+        
         var enemiesInRange = EnemiesInRange();
 
         if (enemiesInRange.Length <= 0) return;
@@ -29,12 +49,27 @@ public class ForceFieldTriggerable : MonoBehaviour
             var dir = enemy.transform.position - transform.position;
 
             var rb = enemy.GetComponent<Rigidbody>();
-            rb.AddForce(dir * hitForce);
+            rb.AddForce(dir.normalized * hitForce);
         }
     }
 
     private Collider[] EnemiesInRange()
     {
         return Physics.OverlapSphere(transform.position, forceFieldRange, targetLayer);
+    }
+    
+    private IEnumerator FieldEffect()
+    {
+        while (timeElapsed < forceFieldDuration)
+        {
+            forceField_sr.transform.localScale = Vector3.Lerp(forceField_sr.transform.localScale, forceFieldMaxScale, timeElapsed / forceFieldDuration);
+
+            yield return null;
+            
+            timeElapsed += Time.deltaTime;
+        }
+
+        timeElapsed = 0;
+        forceField_sr.transform.localScale = forceFieldDefaultScale;
     }
 }
