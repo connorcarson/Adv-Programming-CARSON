90baccb8f41969a80bb2b8c85a263bf225075b52
Connor <connor@glowup.games>
2020-04-12 15:22:40 -0400

    Created abstract ability class derived from Scriptable Object, and a force field ability class, derived from base ability class. Going to create a monobehavior to handle the actual logic of the force field next.

--- Soccer_Project/Assets/Scripts/Ability.cs
+++ Soccer_Project/Assets/Scripts/Ability.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 
 public abstract class Ability : ScriptableObject
 {
-    
+    public string abilityName = "New Ability";
+    public Sprite abilitySprite;
+    public AudioClip abilitySound;
+    public float baseCoolDown = 1f;
+    public float baseCost = 1f;
+
+    public abstract void Initialize(GameObject obj);
+    public abstract void TriggerAbility();
 }
---
+++ Soccer_Project/Assets/Scripts/ForceFieldAbility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Abilities/ForceFieldAbility")]
+public class ForceFieldAbility : Ability
+{
+    public float forceFieldRange = 1.0f;
+    public float hitForce = 10f;
+    public Color forceFieldColor = Color.white;
+
+    public override void Initialize(GameObject obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TriggerAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+}
