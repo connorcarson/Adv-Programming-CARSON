56b5be28c4700729c99b58b1e89f6046775cb7cf
Connor <connor@glowup.games>
2020-04-17 20:24:19 -0400

    Did some research/reading/youtubing on A* and smoothing paths. Currently implementing a simple demo from a tutorial.

---
+++ Pathfinding_Example/Assets/Scripts/Grid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _gridWorldSize;
+
+    [SerializeField] 
+    public float _nodeRadius;
+
+    [SerializeField] 
+    private LayerMask _unwalkableLayer;
+    
+    private Node[,] _grid;
+
+    private void Start()
+    {
+        
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(_gridWorldSize.x, 1, _gridWorldSize.y));
+    }
+}
